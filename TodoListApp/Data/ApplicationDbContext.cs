eaecd93928f4705e4d89dc1fa4d58b77bf92c781
mhdhfz <mhd.hfzz@gmail.com>
2021-08-31 13:17:55 +0800

    remove unnecessary, create dbcontext and database

--- TodoListApp/Data/ApplicationDbContext.cs
+++ TodoListApp/Data/ApplicationDbContext.cs
@@ -4,11 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoListApp.Models;
 
 namespace TodoListApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
-    }
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Todo> Todos { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Status> Statuses { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Category>().HasData(
+				new Category { CategoryId = "work", Name = "Work"},
+				new Category { CategoryId = "home", Name = "Home"},
+				new Category { CategoryId = "exercise", Name = "Exercise"},
+				new Category { CategoryId = "shop", Name = "Shopping"},
+				new Category { CategoryId = "contact", Name = "Contact"}
+			);
+
+			modelBuilder.Entity<Status>().HasData(
+				new Status { StatusId = "open", Name = "Open"},
+				new Status { StatusId = "closed", Name = "Completed"}
+			);
+		}
+	}
 }
