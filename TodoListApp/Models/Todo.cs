197a17e9135df60cbd6a79b08ffc4065127f14ae
mhdhfz <mhd.hfzz@gmail.com>
2021-08-31 08:36:38 +0800

    add category, status and todo model

---
+++ TodoListApp/Models/Todo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    public class Todo
+    {
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Please enter description")]
+		public string Description { get; set; }
+
+		[Required(ErrorMessage = "Please enter a due date")]
+		[DataType(DataType.Date)]
+		public DateTime? DueDate { get; set; }
+
+		[Required(ErrorMessage = "Please select a Category")]
+		public string CategoryId { get; set; }
+		public Category Category { get; set; }
+
+		[Required(ErrorMessage = "Please select a status")]
+		public string StatusId { get; set; }
+		public Status Status { get; set; }
+
+		public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+
+
+	}
+}
