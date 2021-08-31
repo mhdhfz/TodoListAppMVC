7f90f325a60816fac185e53ffeacf988c15c2f49
mhdhfz <mhd.hfzz@gmail.com>
2021-08-31 08:25:55 +0800

    remove if statement of useDeveloperExceptionPage, authorization. add lowercaseurl and appendtrailingslash

--- TodoListApp/Startup.cs
+++ TodoListApp/Startup.cs
@@ -23,28 +23,21 @@ namespace TodoListApp
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddRouting(options =>
+			{
+				options.LowercaseUrls = true;
+				options.AppendTrailingSlash = true;
+			});
 			services.AddControllersWithViews();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+			app.UseDeveloperExceptionPage();
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-
 			app.UseRouting();
-
-			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
